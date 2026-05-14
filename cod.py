7f9653651c8c3c0f6369cb6ccf47a94b30ddfcbf
tomas m <mejiabolivar112@gmail.com>
2026-05-14 14:35:26 -0500

    Base inicial del proyecto

---
+++ cod.py
@@ -0,0 +1,20 @@
+gastos = []
+while True:
+    print("MENU")
+    print("1. Registrar gasto")
+    print("2. Total gastos")
+    print("3. Buscar gastos por placa")
+    print("4. Salir")
+
+    opcion = input("Seleccione una opción: ")
+    if opcion == "1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion == "3":
+        pass
+    elif opcion == "4":
+        print("Programa finalizado")
+        break
+    else:
+        print("Opcion invalida")
